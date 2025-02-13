43e7a8eb3b5d921cf84756fa01ed6299210227ce
gvitorlima <git624886@gmail.com>
2025-02-13 15:23:09 -0300

    feat: .env and validation in config.schema

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,6 +3,22 @@ import { z } from 'zod';
 export const configSchema = z.object({
   PORT: z.coerce.number().int(),
   DATABASE_URL: z.string(),
+  JWT_SECRET: z.string().min(12),
+  JWT_ISS: z.string().nullable().default('flow-track'),
+  JWT_ALG: z.enum([
+    'HS256',
+    'HS384',
+    'HS512',
+    'RS256',
+    'RS384',
+    'RS512',
+    'ES256',
+    'ES384',
+    'ES512',
+    'PS256',
+    'PS384',
+    'PS512',
+  ]),
 });
 
 export type configSchema = z.infer<typeof configSchema>;
