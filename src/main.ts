e08482546ce2d47a45a42dc26c2c1b9a8c3fee88
gvitorlima <git624886@gmail.com>
2025-02-14 16:45:33 -0300

    chore: swagger and docs from create user

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
 import { ResponseDto } from './common/dto/responses/response.dto';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('api docs')
+    .setDescription('The Lucas API description')
+    .setVersion('1.0')
+    .addTag('docs-v1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({
--- src/user/dto/create.user.dto.ts
+++ src/user/dto/create.user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -8,17 +9,24 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsEmail()
   readonly email: string;
 
+  @ApiProperty()
   @IsString()
   @Length(8)
   readonly password: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly name: string;
 
+  @ApiProperty({
+    description:
+      'object with user preferences for example theme, languand and more.',
+  })
   @IsObject()
   @IsOptional()
   readonly preferences?: object;
