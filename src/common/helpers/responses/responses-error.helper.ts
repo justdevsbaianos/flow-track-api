2e73bf894b81183fb2d964b0646f9f75f473204e
gvitorlima <git624886@gmail.com>
2025-02-14 16:31:54 -0300

    fix: error helper using a response error dto

--- src/common/helpers/responses/responses-error.helper.ts
+++ src/common/helpers/responses/responses-error.helper.ts
@@ -1,18 +1,26 @@
 import { Logger } from '@nestjs/common';
-import { ErrorResponseDto } from 'src/common/dto/responses/response-error.dto';
-import { ZodError } from 'zod';
+import { ResponseDto } from 'src/common/dto/responses/response.dto';
+import { number, ZodError } from 'zod';
 
 export class ErrorHelper {
   private static logger = new Logger();
 
   static throwValidationError(error: ZodError): never {
-    throw new ErrorResponseDto(400, 'Validation Error', error.format());
+    throw new ResponseDto(400, 'Validation Error', error.format());
   }
 
   /**
    * TODO - Finalizar classe Helper
    */
-  static throwCustomError(code: number, message: string, details?: any) {
+  static throwCustomError(code: number, message: string, details?: any): never {
     this.logger.log(message);
+
+    if (code >= 500) {
+      this.logger.error(message);
+    } else {
+      this.logger.debug(message);
+    }
+
+    throw new ResponseDto(code, message, details);
   }
 }
